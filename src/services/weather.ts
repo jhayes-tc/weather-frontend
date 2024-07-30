84c040f3e5dc6674f215c2511f84f020bd995d20
Jake Hayes <jhayes@tcetra.com>
2024-07-30 15:33:20 -0400

    Remove space

--- src/services/weather.ts
+++ src/services/weather.ts
@@ -1,14 +1,14 @@
 import { WeatherResponse } from "@/types";
 
-const apiKey = process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY; 
+const apiKey = process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY;
 
 /**
  * Get the current weather for the provided latitude and longitude
  * @param lat: Latitude
- * @param  l on: Longitude  
+ * @param  lon: Longitude  
  * @returns current weather for location
  */
 export async function getWeather(): Promise<WeatherApiResponse> {
-// URL To get data: https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}
+  // URL To get data: https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}
   // TODO: Finish the function
 }
