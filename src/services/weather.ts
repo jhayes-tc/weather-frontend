7793e1c1f376f7d70b1c8dfa9eecabc730c31a30
Jake Hayes <jhayes@tcetra.com>
2024-07-30 15:17:30 -0400

    Remove api key

--- src/services/weather.ts
+++ src/services/weather.ts
@@ -1,14 +1,16 @@
 import { WeatherResponse } from "@/types";
 
-const apiKey = process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY;
-
 /**
  * Get the current weather for the provided latitude and longitude
  * @param lat: Latitude
  * @param  lon: Longitude  
  * @returns current weather for location
  */
-export async function getWeather(): Promise<WeatherApiResponse> {
+export async function getWeather(lat: string, lon: string): Promise<AxiosResponse<WeatherResponse>> {
   // URL To get data: https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}
   // TODO: Finish the function
+  const apiKey = process.env.OPEN_WEATHER_API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+  console.log('URL', url);
+  return api.get(url);
 }
