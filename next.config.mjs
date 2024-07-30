83da44cf63496232bf583cb643860d4ae4b0e546
Jake Hayes <jhayes@tcetra.com>
2024-07-30 15:30:20 -0400

    More cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_OPEN_WEATHER_API_KEY: process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY,
+  },
 };
 
 export default nextConfig;
--- src/services/weather.ts
+++ src/services/weather.ts
@@ -1,12 +1,14 @@
 import { WeatherResponse } from "@/types";
 
+const apiKey = process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY; 
+
 /**
  * Get the current weather for the provided latitude and longitude
  * @param lat: Latitude
- * @param lon: Longitude
+ * @param  l on: Longitude  
  * @returns current weather for location
  */
-export async function getWeather() {
-  // URL To get data: https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}
+export async function getWeather(): Promise<WeatherApiResponse> {
+// URL To get data: https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}
   // TODO: Finish the function
 }
